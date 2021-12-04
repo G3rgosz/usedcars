40a0379a00b1ab8072cb8d41f91a63585a036dd7
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2021-12-04 11:07:33 +0100

    projekt letrehozva

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+export class Car {
+    id: number;
+    plate: string;
+    color: string;
+    brand: string;
+    year: number;
+    capacity: number;
+    fuel: string;
+    price: number;
+    sold: boolean
+}
