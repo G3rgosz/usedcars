260f65773e08cba1a946cf3203f4bb11e1cdaf62
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2021-12-04 12:26:04 +0100

    A program működik, fejrészek rendben

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
-"use strict";
-exports.__esModule = true;
-exports.Car = void 0;
-var Car = /** @class */ (function () {
-    function Car() {
-    }
-    return Car;
-}());
-exports.Car = Car;
+/*
+* File: app.ts/js
+* Author: Szivák Gergő
+* Copyright: 2021, Szivák Gergő
+* Group: Szoft II N
+* Date: 2021-12-04
+* Github: https://github.com/Gergosz-2000/usedcars
+* Licenc: GNU GPL
+*/
+var url = 'http://localhost:3000/cars';
+/*
+Nem jöttem rá, hogy ezt mire kéne használnom....
+class Car {
+    id: number;
+    plate: string;
+    color: string;
+    brand: string;
+    year: number;
+    capacity: number;
+    fuel: string;
+    price: number;
+    sold: boolean
+}*/
+fetch(url).then(function (response) { return response.json(); }).then(function (result) {
+    console.log(result);
+    var list = document.querySelector('#list');
+    result.forEach(function (Car) {
+        var li = document.createElement('li');
+        li.innerHTML = Car.plate;
+        li.setAttribute('class', 'list-group-item');
+        list.append(li);
+    });
+});
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
-export class Car {
+/*
+* File: app.ts/js
+* Author: Szivák Gergő
+* Copyright: 2021, Szivák Gergő
+* Group: Szoft II N
+* Date: 2021-12-04
+* Github: https://github.com/Gergosz-2000/usedcars
+* Licenc: GNU GPL
+*/
+var url = 'http://localhost:3000/cars';
+
+/*
+Nem jöttem rá, hogy ezt mire kéne használnom....
+class Car {
     id: number;
     plate: string;
     color: string;
@@ -8,4 +21,15 @@ export class Car {
     fuel: string;
     price: number;
     sold: boolean
-}
+}*/
+
+fetch(url).then(response => response.json()).then(result => {
+    console.log(result);
+    let list = document.querySelector('#list');
+    result.forEach(Car => {
+        let li = document.createElement('li');
+        li.innerHTML = Car.plate;
+        li.setAttribute('class', 'list-group-item');
+        list.append(li);
+    });
+});
